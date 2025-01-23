76d512ee2980a4389f56b7574ec114fe6a90f04f
ravindra_gouda <ravindragouda444@gmail.com>
2025-01-23 07:00:00 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -4,45 +4,53 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My todos</h1>
+      <h1>My Todos</h1>
+
       <div className="todo-wrapper">
         <div className="todo-input">
-          <div>
-            <label>
-              Title
-            </label>
-            <input type="text" placeholder="enter your title here"></input>
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input
+              type="text"
+
+
+              placeholder="What's the task title?"
+            />
           </div>
-        </div>
-        <div className="todo-input">
-          <div>
-            <label>
-              Description
-            </label>
-            <input type="text" placeholder="enter your Description here"></input>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input
+              type="text"
+
+
+              placeholder="What's the task description?"
+            />
           </div>
-        </div>
-        <div className="todo-input">
-          <div>
-            <button type="add" className="primary-btn">Add</button>
+          <div className="todo-input-item">
+            <button
+              type="button"
+
+              className="primaryBtn"
+            >
+              Add
+            </button>
           </div>
         </div>
         <div className="btn-area">
-          <div>
             <button>Todo</button>
             <button>Completed</button>
-          </div>
-          
-
         </div>
         <div className="todo-list">
-          <h1>Task 1</h1>
-          <p>Description 1</p>
-
+            <div className="todo-list-item">
+              <h3>Task 1</h3>
+              <p>Description</p>
+            </div>
         </div>
 
-      </div>
 
+
+
+      </div>
     </div>
   );
 }
