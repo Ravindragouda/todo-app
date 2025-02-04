e0b900b55b6f0eb386d7193264a438a224bd6af1
ravindra_gouda <ravindragouda444@gmail.com>
2025-02-04 09:14:40 +0530

    added delete and check icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import {AiOutlineDelete, AiOutlineEdit} from 'react-icons/ai';
+import {BsCheckLg} from 'react-icons/bs';
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState (false);
   return (
     <div className="App">
       <h1>My Todos</h1>
@@ -37,14 +41,24 @@ function App() {
           </div>
         </div>
         <div className="btn-area">
-            <button>Todo</button>
-            <button>Completed</button>
+          <button className={`secondaryBtn ${isCompleteScreen === false && 'active'}`}
+            onClick={() => setIsCompleteScreen (false)}>Todo</button>
+          <button className={`secondaryBtn ${isCompleteScreen === true && 'active'}`}
+            onClick={() => setIsCompleteScreen (true)}>Completed</button>
         </div>
         <div className="todo-list">
-            <div className="todo-list-item">
-              <h3>Task 1</h3>
-              <p>Description</p>
+          <div className="todo-list-item">
+            <div>
+            <h3>Task 1</h3>
+            <p>Description</p>
             </div>
+            
+            <div>
+             <AiOutlineDelete className="icon"/>
+             <BsCheckLg className="check-icon" />
+          </div>
+          </div>
+       
         </div>
 
 
